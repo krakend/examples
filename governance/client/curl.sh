60ad60926cd78a74453cf3578dab2dc97c362b3d
David Hontecillas <dhontecillas@gmail.com>
2025-06-03 10:23:53 +0200

    quotas example: use backend static filesystem to mock consume tokens response

--- governance/client/curl.sh
+++ governance/client/curl.sh
@@ -2,16 +2,34 @@
 
 KRAKEND_ADDRESS='http://localhost:8080'
 
+# Number of requests to make
+NUM_REQUESTS=1
+
+# Endpoint to call
+# QUOTAS_ENDPOINT='/request_weapon_power_up'
+QUOTAS_ENDPOINT='/request_squad_call'
+# QUOTAS_ENDPOINT='/consume_life'
+
+# Player to use
+QUOTAS_USER=bart
+# QOTAS_USER=homer
+
+# Select the game to use;
 QUOTAS_GAME=galaga
-QUOTAS_GAME=space_harrier
-QUOTAS_GAME=r_type
+# QUOTAS_GAME=space_harrier
+# QUOTAS_GAME=r_type
 
-echo "Making request, showing headers..."
+QUOTAS_GAME=galaga
+# QUOTAS_GAME=space_harrier
+# QUOTAS_GAME=r_type
 
-for i in $(seq 1 10);
+echo "Making ${NUM_REQUESTS} request(s), showing headers..."
+
+for i in $(seq 1 ${NUM_REQUESTS});
 do
+    echo -e "\n${i}:\n"
     curl -v \
-        -H 'Authorization: bart' \
-        -H 'X-Game: sonic_the_hedgehog' \
-        "${KRAKEND_ADDRESS}/consume_life"
+        -H "Authorization: ${QUOTAS_USER}" \
+        -H "X-Game: ${QUOTAS_GAME}" \
+        "${KRAKEND_ADDRESS}${QUOTAS_ENDPOINT}"
 done
