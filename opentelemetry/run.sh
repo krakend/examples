fb09257b28623f432bd713a04bfdeb174dedf2cb
David Hontecillas <dhontecillas@gmail.com>
2025-12-01 19:22:52 +0100

    wip: update to latest krakend version

--- opentelemetry/run.sh
+++ opentelemetry/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+
+export VERSION=2.12.0
+docker pull krakend/krakend-ee:${VERSION}
+
 if [ -z $DD_API_KEY ]; then
     echo -e "\nNo DD_API_KEY found. Set it with:\n\n    export DD_API_KEY="YOUR_API_KEY"\n"
     exit -1
