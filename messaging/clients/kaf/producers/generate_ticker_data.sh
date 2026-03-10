f6aeef0ea8267242f3319aedc36f1a62121e4307
kpacha <dlopez@krakend.io>
2026-03-10 15:38:12 +0100

    host should be localhost, by now
    Signed-off-by: kpacha <dlopez@krakend.io>

--- messaging/clients/kaf/producers/generate_ticker_data.sh
+++ messaging/clients/kaf/producers/generate_ticker_data.sh
@@ -3,7 +3,7 @@
 # this script produces fake ticker prices, using the `prices.txt` data
 # with each line for each of the list of tickers
 KTOPIC=stockprice
-KADDRESS=shrimp.ln:9092
+KADDRESS=localhost:9092
 
 echo -e "Producing: $KKEY into '$KTOPIC' topic\n" 
 
